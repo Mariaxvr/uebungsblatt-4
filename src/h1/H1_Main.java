10e0f0c44e49498df3d206ca781ce2de019c3420
Mariaxvr <mariavkrohnq@gmail.com>
2025-11-13 22:22:07 +0100

    hausaufgabe 4

--- src/h1/H1_Main.java
+++ src/h1/H1_Main.java
@@ -3,19 +3,31 @@ package h1;
 public class H1_Main {
     public static void main(String[] args) {
 
-        int zone = 3;
-        double price = 2.00;
+        int zone = 7;
+        double price ;
 
-        switch(zone) {
-            case 1: System.out.println(price); break;
-            case 2: System.out.println(price + 0.35); break;
-            case 3,4: System.out.println(price + 0.85); break;
-            case 5: System.out.println(price + 1.55); break;
-            case 6: System.out.println(price + 2.00); break;
-            default: System.out.println(price + 2.00); break;
+        switch (zone) {
+            case 1:
+                price = 2.00;
+                break;
+            case 2:
+                price = 2.35;
+                break;
+            case 3, 4:
+                price = 2.85;
+                break;
+            case 5:
+                price = 3.55;
+                break;
+            case 6:             // kann man auch weglassen
+                price = 4.00;
+                break;
+            default:
+                price = 4.00;
+                break;
         }
 
-
+        System.out.println("Zone: " + zone + " -> Price: " + price + "€");
 
     }
 }
